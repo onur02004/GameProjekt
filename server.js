085ab1bbe5e35d1b16e82b150b777a1db908c075
dominik <Kannix1402@gmail.com>
2025-12-12 16:32:09 +0100

    besser port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const PORT = 3000;
+const PORT = 6769;
 
 app.use(cors());
 app.use(express.json());
